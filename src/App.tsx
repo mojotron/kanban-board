08741ee4447213278810b9e34e5cbba53832b8cd
mojo <igormalj@gmail.com>
2023-06-23 23:09:14 +0200

    add tensteck router

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,26 @@
-<<<<<<< HEAD
-import { useLogin } from './hooks/useLogin';
-import { useLogout } from './hooks/useLogout';
-import Login from './pages/Login/Login';
-
-const buttonStyle = {
-  border: '1px solid white',
-  background: 'none',
-  padding: '10px 20px',
-  color: 'white',
-  cursor: 'pointer',
-};
-
-const App = () => {
-  const { login, error, isPending } = useLogin();
-  const { logout } = useLogout();
-
-  return (
-    <div className="App">
-      <Login />
-
-      <button style={buttonStyle} className="btn" onClick={login}>{`${
-        isPending ? 'Loading...' : 'Log in with Github'
-      }`}</button>
-      <button style={buttonStyle} className="btn" onClick={logout}>
-        Log Out
-      </button>
-=======
+import { Router, RouterProvider } from '@tanstack/router';
+// pages
 import KanbanBoard from './pages/KanbanBoard/KanbanBoard';
 
+const routes = [
+  {
+    path: '/',
+    element: (
+      <>
+        <KanbanBoard />
+      </>
+    ),
+  },
+];
+
+const router = new Router(routes);
+
 const App = () => {
   return (
     <div className="App">
-      <KanbanBoard />
->>>>>>> 0dfd5d1 (add basic layout for columns)
+      <RouterProvider router={router}>
+        <KanbanBoard />
+      </RouterProvider>
     </div>
   );
 };
