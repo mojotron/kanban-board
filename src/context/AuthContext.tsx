29a9114f2b742cbe8237d48ae5517a32be6f48bf
mojo <igormalj@gmail.com>
2023-07-03 23:08:35 +0200

    refactor auth context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,32 +1,49 @@
 import { User } from 'firebase/auth';
-import { ReactNode, createContext, useReducer } from 'react';
+import { ReactNode, createContext, useContext, useReducer } from 'react';
 
-export const AuthContext = createContext({ authIsReady: false, user: null });
-
-type PropsType = {
-  children: ReactNode;
-};
-const authReducer = (state, action) => {
-  switch (action.type) {
-    case 'LOGIN':
-      return { ...state, user: action.payload };
-    case 'LOGOUT':
-      return { ...state, user: null };
-    case 'AUTH_IS_READY':
-      return { ...state, user: action.payload, authIsReady: true };
-    default:
-      return { ...state };
-  }
-};
-
-export const AuthContextProvider = ({ children }: PropsType) => {
-  const [state, dispatch] = useReducer<{
+const useAuthSource = () => {
+  type AuthState = {
     authIsReady: boolean;
     user: null | User;
-  }>(authReducer, { authIsReady: false, user: null });
+  };
 
+  type AuthActions =
+    | { type: 'LOGIN'; payload: User }
+    | { type: 'LOGOUT'; payload: null }
+    | { type: 'AUTH_IS_READY'; payload: User };
+
+  const authReducer = (state: AuthState, action: AuthActions) => {
+    switch (action.type) {
+      case 'LOGIN':
+        return { ...state, user: action.payload };
+      case 'LOGOUT':
+        return { ...state, user: null };
+      case 'AUTH_IS_READY':
+        return { ...state, user: action.payload, authIsReady: true };
+      default:
+        return { ...state };
+    }
+  };
+
+  const [{ user, authIsReady }, dispatch] = useReducer(authReducer, {
+    authIsReady: false,
+    user: null,
+  });
+
+  return { user, authIsReady, dispatch };
+};
+
+const AuthContext = createContext<ReturnType<typeof useAuthSource>>(
+  {} as unknown as ReturnType<typeof useAuthSource>
+);
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <AuthContext.Provider value={...(state, dispatch)}>
+    <AuthContext.Provider value={useAuthSource()}>
       {children}
     </AuthContext.Provider>
   );
