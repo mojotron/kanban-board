3dd6beb8cc433eb9e1b8bf6a8580ca27d6f795fe
mojo <igormalj@gmail.com>
2023-07-08 21:29:06 +0200

    add styles for new project form

--- src/pages/DashboardPage/components/NewProjectForm.tsx/NewProjectForm.tsx
+++ src/pages/DashboardPage/components/NewProjectForm.tsx/NewProjectForm.tsx
@@ -1,32 +1,64 @@
 import './NewProjectForm.css';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 const NewProjectForm = () => {
   return (
-    <form className="NewProjectForm">
-      <h2 className="heading--secondary">New Project</h2>
+    <div className="overlay">
+      <form className="NewProjectForm">
+        <button className="btn--icon" type="button">
+          <AiOutlineCloseCircle size={30} color="var(--COLOR-ACCENT-500)" />
+        </button>
 
-      <label className="NewProjectForm__label">
-        Project name
-        <input type="text" />
-      </label>
-      <label className="NewProjectForm__label">
-        Project description
-        <textarea />
-      </label>
-      <label className="NewProjectForm__label">
-        Tags
-        <input type="text" />
-        <button type="button">Add</button>
-      </label>
-      <label className="NewProjectForm__label">
-        Project repository
-        <input type="text" />
-      </label>
-
-      <button type="submit" className="btn">
-        create
-      </button>
-    </form>
+        <h2 className="heading--secondary">New Project</h2>
+        <div className="NewProjectForm__item">
+          <label htmlFor="new-project-name">Project Name</label>
+          <input
+            id="new-project-name"
+            type="text"
+            placeholder="My Secret Project"
+            required
+            maxLength={100}
+          />
+        </div>
+        <div className="NewProjectForm__item">
+          <label htmlFor="new-project-description">Project description</label>
+          <textarea
+            id="new-project-description"
+            placeholder="write short description what is project about"
+            required
+            maxLength={2200}
+          />
+        </div>
+        <div className="NewProjectForm__item">
+          <label htmlFor="project-tags">Tags</label>
+          <div className="NewProjectForm__item__wrapper">
+            <input
+              id="project-tags"
+              type="text"
+              placeholder="project keywords e.g. 'react'"
+              maxLength={35}
+            />
+            <button type="button" className="btn">
+              Add
+            </button>
+          </div>
+        </div>
+        <div className="NewProjectForm__item mb--lg">
+          <label htmlFor="new-project-repo">Project repository</label>
+          <input
+            id="new-project-repo"
+            type="text"
+            placeholder="projects github page"
+          />
+        </div>
+        <button type="submit" className="btn">
+          create
+        </button>
+        <p className="NewProjectForm__note">
+          You can update all fields later in your dashboard!
+        </p>
+      </form>
+    </div>
   );
 };
 
