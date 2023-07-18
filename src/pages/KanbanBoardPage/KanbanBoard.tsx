2354af8e0bd8234396e62713d6b330065f2e95ef
mojo <igormalj@gmail.com>
2023-07-18 21:29:51 +0200

    minor clean up for kanban board part

--- src/pages/KanbanBoardPage/KanbanBoard.tsx
+++ src/pages/KanbanBoardPage/KanbanBoard.tsx
@@ -1,16 +1,14 @@
 import './KanbanBoard.css';
-import { COLUMNS } from '../../constants/taskStages';
+import { TASK_STAGES } from '../../constants/taskStages';
 
 import Column from './components/Column/Column';
 
 const KanbanBoard = () => {
   return (
     <div className="KanbanBoard">
-      <Column title={COLUMNS.BACKLOG} />
-      <Column title={COLUMNS.ASSIGNMENT} />
-      <Column title={COLUMNS.DEVELOPMENT} />
-      <Column title={COLUMNS.TEST} />
-      <Column title={COLUMNS.COMPLETE} />
+      {TASK_STAGES.map((stage) => (
+        <Column title={stage} />
+      ))}
     </div>
   );
 };
--- src/pages/KanbanBoardPage/components/Task/Task.test.tsx
+++
@@ -1,7 +0,0 @@
-import { describe, test, expect } from 'vitest';
-import { render } from '@testing-library/react';
-import Task from './Task';
-
-describe('KanbanBoar page - Task component', () => {
-  test('happy path', () => {});
-});
