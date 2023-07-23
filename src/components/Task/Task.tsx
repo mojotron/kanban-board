9b1c16a6f3c0d15ae549ade9b41158c401f644dc
mojo <igormalj@gmail.com>
2023-07-23 19:02:07 +0200

    add task controls and textbox component

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,11 +1,17 @@
+// style & icons
 import './Task.css';
+import { AiOutlineUserAdd, AiFillEdit, AiFillEye } from 'react-icons/ai';
+// types
 import { TaskType } from '../../types/taskType';
-import Avatar from '../Avatar/Avatar';
-import { useOnSnapshotDocument } from '../../hooks/useOnSnapshotDocument';
-import { AiOutlineUserAdd } from 'react-icons/ai';
 import { UserType } from '../../types/userType';
+// components
+import Avatar from '../Avatar/Avatar';
+// helpers
 import { formatTime } from '../../utils/formatTime';
+// hooks
+import { useOnSnapshotDocument } from '../../hooks/useOnSnapshotDocument';
 import { useMemo } from 'react';
+import TextBox from '../TextBox/TextBox';
 
 type Props = {
   taskData: TaskType & { id: string };
@@ -25,31 +31,48 @@ const Task = ({ taskData }: Props) => {
       overDue: (+new Date() - taskData.deadline.seconds * 1000) / 1000,
     };
   }, [taskData.deadline]);
-  //
 
   return (
     <article className="Task">
       <header className={`Task__header priority--${taskData.priority}`}>
         <h3 className="Task__header__heading">{taskData.title}</h3>
-        {document ? (
-          <Avatar
-            imageUrl={document?.photoUrl}
-            userName={document?.userName}
-            size="50"
-          />
-        ) : (
-          <button className="Task__add-button">
-            <AiOutlineUserAdd size={35} color={'var(--COLOR-GRAY-500)'} />
+
+        <div className="Task__header__controls">
+          <button className="Task__btn" title="inspect task">
+            <AiFillEye size={25} />
           </button>
-        )}
+          <button className="Task__btn" title="edit task">
+            <AiFillEdit size={25} />
+          </button>
+          {document ? (
+            <Avatar
+              imageUrl={document?.photoUrl}
+              userName={document?.userName}
+              size="50"
+            />
+          ) : (
+            <button className="Task__btn Task__btn--add" title="pick up task">
+              <AiOutlineUserAdd size={35} color={'var(--COLOR-GRAY-500)'} />
+            </button>
+          )}
+        </div>
       </header>
       <main className={`Task__body`}>
         {deadline && (
-          <p style={{ color: deadline.overDue > 0 ? 'red' : 'green' }}>
-            Deadline: {deadline.formatted},{' '}
-          </p>
+          <span
+            className={`Task__body__deadline Task__body__deadline--${
+              deadline.overDue > 0 ? 'red' : 'green'
+            }`}
+          >
+            <span className="Task__body__deadline__heading">Deadline:</span>{' '}
+            <span>{deadline.formatted}</span>
+          </span>
         )}
-        <p className="Task__body__description">{taskData.description}</p>
+        <TextBox
+          text={taskData.description}
+          length={50}
+          className="Task__body__description"
+        />
       </main>
     </article>
   );
