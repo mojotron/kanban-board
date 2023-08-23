14a7e34ba9324dd712b2653ce0f98fbd7b93c7b3
mojo <igormalj@gmail.com>
2023-08-23 21:54:42 +0200

    add basic style for messages

--- src/pages/Dashboard/components/PojectMessages/ProjectMessages.tsx
+++ src/pages/Dashboard/components/PojectMessages/ProjectMessages.tsx
@@ -1,21 +1,24 @@
 import EmojiPicker from 'emoji-picker-react';
 import { useState } from 'react';
 import { AiOutlineSmile, AiOutlineArrowRight } from 'react-icons/ai';
+import './ProjectMessages.css';
 
 const ProjectMessages = () => {
   const [showEmojis, setShowEmojis] = useState(false);
   return (
-    <div className="Dashboard__messages">
+    <div className="ProjectMessages">
       <h3 className="heading--tertiary">Messages</h3>
-      <div className="Dashboard__messages__body"></div>
+
+      <div className="ProjectMessages__messages"></div>
+
       {showEmojis && <EmojiPicker />}
-      <div className="Dashboard__messages__controls">
+      <div className="ProjectMessages__controls">
         <button onClick={() => setShowEmojis((oldValue) => !oldValue)}>
-          <AiOutlineSmile size={25} />
+          <AiOutlineSmile size={25} color="var(--COLOR-GRAY-100)" />
         </button>
-        <input type="text" placeholder="Type a message..." />
+        <textarea placeholder="Type a message..." />
         <button>
-          <AiOutlineArrowRight size={25} />
+          <AiOutlineArrowRight size={25} color="var(--COLOR-GRAY-100)" />
         </button>
       </div>
     </div>
