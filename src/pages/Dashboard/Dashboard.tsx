b1edaa0b8052f192fdbb3ea750f2e041013d71a4
mojo <igormalj@gmail.com>
2023-09-16 22:00:13 +0200

    add styles to project menu

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,53 +1,33 @@
-// import './Dashboard.css';
-import { useMemo } from 'react';
-// global store
+// hooks
 import { useKanbanStore } from '../../store';
+import { useProject } from '../../context/ProjectContext';
+import { useFirestore } from '../../hooks/useFirestore';
 // components
 import NewTaskForm from './components/NewTaskForm/NewTaskForm';
-import TaskCard from '../../components/TaskCard/TaskCard';
 import Task from '../../components/Task/Task';
-import PriorityLegend from '../../components/PriorityLegend/PriorityLegend';
 import ConfirmPopup from '../../components/ConfirmPopup/ConfirmPopup';
-// constants and types
-import { TaskType } from '../../types/taskType';
-import { TASK_STAGES } from '../../constants/taskStages';
-// use context hooks
-import { Link } from 'react-router-dom';
-import { useUserData } from '../../context/UserDataContext';
-import { useProject } from '../../context/ProjectContext';
 import TeamMembers from './components/TeamMembers/TeamMembers';
 import ProjectMessages from './components/PojectMessages/ProjectMessages';
-import ExpandedText from '../../components/ExpandedText/ExpandedText';
-import { useFirestore } from '../../hooks/useFirestore';
-import Button from '../../components/Button/Button';
-// refactor
 import ProjectMenu from './components/ProjectMenu/ProjectMenu';
 import Description from './components/Description/Description';
 import Tasks from './components/Tasks/Tasks';
-
-type Task = TaskType & { id: string };
+// types
+import { TaskType } from '../../types/taskType';
+// styles
+import styles from './Dashboard.module.css';
 
 const Dashboard = () => {
   const { updateDocument } = useFirestore();
-  const currentTaskStage = useKanbanStore((state) => state.currentTaskStage);
+
   const openNewTaskModal = useKanbanStore((state) => state.openNewTaskModal);
   const setOpenNewTaskModal = useKanbanStore(
     (state) => state.setOpenNewTaskModal
   );
-  const setCurrentTaskStage = useKanbanStore(
-    (state) => state.setCurrentTaskStage
-  );
+
   const openViewTaskModal = useKanbanStore((state) => state.openViewTaskModal);
   const openConfirmModal = useKanbanStore((state) => state.openConfirmModal);
 
-  const { document: user } = useUserData();
-  const { project, projectErr, projectPending, tasks, tasksPending, tasksErr } =
-    useProject();
-
-  const filteredStageTasks = useMemo((): Task[] | undefined => {
-    if (!tasks) return undefined;
-    return tasks.filter((task) => task.stage === currentTaskStage);
-  }, [tasks, currentTaskStage]);
+  const { project, projectErr, projectPending } = useProject();
 
   const handlePublicToggle = async () => {
     if (!project) return;
@@ -55,7 +35,7 @@ const Dashboard = () => {
   };
 
   return (
-    <main className="Dashboard">
+    <main className={styles.dashboard}>
       {projectPending && <h2>Loading...</h2>}
       {projectErr && <h2 className="error">{projectErr}</h2>}
 
@@ -67,94 +47,10 @@ const Dashboard = () => {
       {project && (
         <>
           <ProjectMenu />
-          <Description />
+          {/* <Description />
           <Tasks />
-          {/* <header className="Dashboard__header">
-            <div className="Dashboard__header__title">
-              <h1>{project.name}</h1>
-              <a
-                className="link"
-                href={project.repository}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                project repository
-              </a>
-
-              <Button handleClick={handlePublicToggle}>
-                {project.public ? 'Go Private' : 'Go Public'}
-              </Button>
-
-              <button className="btn">Delete Project</button>
-            </div>
-            <div className="Dashboard__header__tags">
-              {project.tags.map((tag) => (
-                <div key={tag} className="tag">
-                  {tag}
-                </div>
-              ))}
-            </div>
-            <ExpandedText
-              text={project.description}
-              hideWordsLength={20}
-              className="Dashboard__header__description"
-            />
-          </header>
-
-          <div className="Dashboard__tasks">
-            <div className="Dashboard__tasks__header">
-              <h2 className="heading--secondary">Tasks</h2>
-              <PriorityLegend />
-              {project.adminUid === user?.uid && (
-                <Button
-                  handleClick={() => {
-                    setOpenNewTaskModal(true);
-                  }}
-                  text="create task"
-                />
-              )}
-            </div>
-
-            <div className="Dashboard__tasks__tabs">
-              {TASK_STAGES.map((stage) => {
-                const countTasks = count(
-                  tasks as Task[],
-                  (ele) => ele.stage === stage
-                );
-
-                return (
-                  <button
-                    onClick={() => setCurrentTaskStage(stage)}
-                    className={`${stage === currentTaskStage ? 'active' : ''}`}
-                    key={stage}
-                  >
-                    {stage}
-                    {countTasks > 0 && `(${countTasks})`}
-                  </button>
-                );
-              })}
-            </div>
-
-            <div className="Dashboard__tasks__display">
-              {tasksPending && <p>Loading...</p>}
-              {tasksErr && <p className="error">{tasksErr}</p>}
-              {filteredStageTasks &&
-                filteredStageTasks.map((task) => (
-                  <TaskCard key={task.id} taskData={task} />
-                ))}
-            </div>
-          </div>
-
           <ProjectMessages />
-
-          <TeamMembers />
-
-          <Link
-            to={`/kanban/${project.id}`}
-            className="btn Dashboard__btn-kanban"
-          >
-            View Kanban Board
-          </Link> */}
+          <TeamMembers /> */}
         </>
       )}
     </main>
