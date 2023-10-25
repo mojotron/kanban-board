f82aa4472b3821f7c4118e49236c19fefbbb6de1
mojo <igormalj@gmail.com>
2023-10-25 20:52:31 +0200

    add first part of geting projeect hook

--- src/features/Search/Search.tsx
+++ src/features/Search/Search.tsx
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
 import SearchFilters from './components/SearchFilters/SearchFilters';
 import SearchResults from './components/SearchResults/SearchResults';
+import { useGetSearchedDocs } from './hooks/useGetSearchedDocs';
 import styles from './Search.module.css';
 
 const Search = () => {
+  const { documents, getFirst } = useGetSearchedDocs(
+    'projects',
+    'mojo',
+    'latest'
+  );
+
+  useEffect(() => {
+    getFirst();
+  }, [getFirst]);
+
+  console.log(documents);
+
   return (
     <main className={styles.searchProjects}>
       <SearchFilters />
--- src/features/Search/hooks/useGetSearchedDocs.tsx
+++ src/features/Search/hooks/useGetSearchedDocs.tsx
@@ -1,26 +1,73 @@
-import { QueryDocumentSnapshot } from 'firebase/firestore';
+import {
+  QueryDocumentSnapshot,
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+} from 'firebase/firestore';
 import { useCallback, useState } from 'react';
+import { firebaseFirestore } from '../../../firebase/config';
 
-const DOC_LIMIT = 5;
+const DOC_LIMIT = 2;
 
-export const useGetSearchedDocs = (
-  queryParam: string,
-  filter: string
+export const useGetSearchedDocs = <T,>(
+  collectionName: string, // from urlParam
+  filter: string | undefined, //from user
+  queryParam: string | undefined // from user
 ): {
   getFirst: () => void;
   getNext: () => void;
   isFetching: boolean;
   error: null | string;
+  documents: undefined | T[];
 } => {
   const [lastDocument, setLastDocument] =
     useState<null | QueryDocumentSnapshot>(null);
   const [endOfDocuments, setEndOfDocuments] = useState(false);
   //
+  const [documents, setDocuments] = useState<undefined | T[]>();
   const [isFetching, setIsFetching] = useState(false);
   const [error, setError] = useState<null | string>(null);
   //
-  const getFirst = useCallback(() => {}, []);
-  const getNext = useCallback(() => {}, []);
+  const getFirst = useCallback(async () => {
+    try {
+      setIsFetching(true);
+      const first = query(
+        collection(firebaseFirestore, collectionName),
+        orderBy('createdAt', 'desc'),
+        limit(DOC_LIMIT)
+      );
+      const docSnapshots = await getDocs(first);
+      if (docSnapshots.empty) {
+        setEndOfDocuments(true);
+      } else {
+        setDocuments(
+          docSnapshots.docs.map((doc) => ({ ...doc.data(), id: doc.id } as T))
+        );
+        setLastDocument(docSnapshots.docs[docSnapshots.docs.length - 1]);
+      }
+      setIsFetching(false);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+        setIsFetching(false);
+      }
+    }
+  }, [collectionName]);
 
-  return { getFirst, getNext, isFetching, error };
+  const getNext = useCallback(async () => {
+    if (endOfDocuments || isFetching) return;
+    try {
+      setIsFetching(true);
+      setIsFetching(false);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+        setIsFetching(false);
+      }
+    }
+  }, []);
+
+  return { getFirst, getNext, isFetching, error, documents };
 };
