c194f4157333e2d5af765fd0f254893efd075108
mojo <igormalj@gmail.com>
2023-12-04 21:37:14 +0100

    Add reducer to useSearch

--- src/features/Search/hooks/useSearch.tsx
+++ src/features/Search/hooks/useSearch.tsx
@@ -1,14 +1,29 @@
 import { useCallback, useEffect, useReducer } from 'react';
 // types
 import type { SearchCollections } from '../types/filterTypes';
+import { DocumentData, QueryDocumentSnapshot } from 'firebase/firestore';
 
 type StateType = {
   filter: string;
   query: string;
+  isFetching: boolean;
+  documents: QueryDocumentSnapshot<DocumentData>[];
+  error: null | string;
+  lastDocument: QueryDocumentSnapshot<DocumentData> | null;
 };
 type ActionType =
   | { type: 'filter/update'; payload: string }
-  | { type: 'query/update'; payload: string };
+  | { type: 'query/update'; payload: string }
+  | { type: 'documents/fetching' }
+  | { type: 'documents/error'; payload: string }
+  | {
+      type: 'documents/load';
+      payload: {
+        documents: QueryDocumentSnapshot<DocumentData>[];
+        lastDocument: QueryDocumentSnapshot<DocumentData>;
+      };
+    }
+  | { type: 'document/end'; payload: boolean };
 
 export const useSearch = (collectionName: SearchCollections | undefined) => {
   // obligatory(url param)| optional(user input)
@@ -20,6 +35,10 @@ export const useSearch = (collectionName: SearchCollections | undefined) => {
           return { ...state, filter: action.payload };
         case 'query/update':
           return { ...state, query: action.payload };
+        case 'documents/fetching':
+          return { ...state, isFetching: true };
+        case 'documents/error':
+          return { ...state, error: action.payload };
         default:
           return { ...state };
       }
@@ -27,11 +46,28 @@ export const useSearch = (collectionName: SearchCollections | undefined) => {
     {
       filter: 'tag',
       query: '',
+      isFetching: false,
+      documents: [],
+      error: null,
+      lastDocument: null,
     }
   );
 
-  const getFirst = () => {};
-  const getNext = () => {};
+  const getFirst = useCallback(async () => {
+    try {
+    } catch (error) {
+      if (error instanceof Error) {
+      }
+    }
+  }, [collectionName]);
+
+  const getNext = useCallback(async () => {
+    try {
+    } catch (error) {
+      if (error instanceof Error) {
+      }
+    }
+  }, [collectionName]);
 
   const updateFilter = useCallback((newFilter: string) => {
     dispatch({ type: 'filter/update', payload: newFilter });
@@ -44,10 +80,12 @@ export const useSearch = (collectionName: SearchCollections | undefined) => {
   useEffect(() => {
     if (query === '') return;
     let timeout: NodeJS.Timer;
+
     const debounce = () => {
       timeout = setTimeout(() => {
         console.log('get data', query);
-      }, 3000);
+        // TODO load first
+      }, 1000);
     };
 
     debounce();
